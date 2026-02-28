c9bc9f6bed3179bba4f294892f7530156008f30e
gimsudal20-prog <gimsudal20@gmail.com>
2026-02-28 12:49:19 +0900

    Update view_keyword.py

--- view_keyword.py
+++ view_keyword.py
@@ -11,7 +11,7 @@ from datetime import date
 from data import *
 from ui import *
 from page_helpers import *
-# ✨ [추가] 언더스코어(_)로 시작하는 함수 명시적 불러오기
+# ✨ [핵심] 언더스코어(_)로 시작하는 함수 명시적 불러오기
 from page_helpers import _perf_common_merge_meta
 
 def page_perf_keyword(meta: pd.DataFrame, engine, f: Dict):
